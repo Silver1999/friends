a37e8f7575c3f48f082c0f9d2b10832e15448518
Silver1999 <misha911007@gmail.com>
2020-04-23 16:19:20 +0300

    generate fake data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,12 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'username' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+//        'email_verified_at' => now(),
+        'password' => bcrypt(Str::random(6)), // password
+        'first_name' => $faker->name,
+        'last_name'=> $faker->name
+
     ];
 });
